c747104f1f34946ca3b52827c98d965bd9e3b58d
Alonzo Dority <alonzodority1@gmail.com>
2020-10-11 14:42:14 -0400

    adding eleciont audit code

---
+++ Membership.py
@@ -0,0 +1,5 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties or "Arapahoe" in counties:
+    print("El Paso or Arapahoe is in the list of counties.")
+else:
+    print("El Paso nor Arapahoe is in the list of counties.")
